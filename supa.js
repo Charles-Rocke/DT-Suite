c53891edbc0c188fabc89e298c2f20f623c5bde4
SpOOk <spook.devv@gmail.com>
2023-10-18 20:33:47 -0400

    No supa on client

--- supa.js
+++ supa.js
@@ -1,7 +1,7 @@
 // supa.js
 // Create a single supabase client for interacting with your database
-const supabaseUrl = "https://enssmnohepficaxcmyjb.supabase.co";
-const supabaseApiKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVuc3Ntbm9oZXBmaWNheGNteWpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY5Nzk4MDgsImV4cCI6MjAxMjU1NTgwOH0.asLCggvlojyU2WlGmlAUAaNjC2LDEYHIyicJynvFkVk";
-// Create Supabase client
-const supabase = createClient(supabaseUrl, supabaseApiKey);
+// const supabaseUrl = "https://enssmnohepficaxcmyjb.supabase.co";
+// const supabaseApiKey =
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVuc3Ntbm9oZXBmaWNheGNteWpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY5Nzk4MDgsImV4cCI6MjAxMjU1NTgwOH0.asLCggvlojyU2WlGmlAUAaNjC2LDEYHIyicJynvFkVk";
+// // Create Supabase client
+// const supabase = createClient(supabaseUrl, supabaseApiKey);
